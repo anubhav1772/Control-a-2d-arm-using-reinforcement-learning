0de94b43844f9c5aeb6ef7a8a937e19becc55a8e
anubhav1772 <anubhav.s@orangewood.co>
2019-08-29 00:34:23 +0530

    updated

--- Viewer.py
+++ Viewer.py
@@ -1,30 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 31 17:56:56 2019
-@author: anubhav Singh
-"""
 import numpy as np
 import pygame 
 # import pygame.locals for easier  
 # access to key coordinates 
 from pygame.locals import *
 
-
-class Env(object):
-    viewer = None
-
-    def __init__(self):
-        pass
-
-    def step(self, action):
-        pass
-
-    def reset(self):
-        pass
-
-    def render(self):
-        pass
-    
 class Viewer():
     def __init__(self, arm_info, goal_info):
         self.arm_info = arm_info
